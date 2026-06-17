b0bcb242a4de5f6e1eaba46a811f6ab816322329
Violet Nonbloosom Reborn <235866125+Violet-Nonbloosom-Reborn@users.noreply.github.com>
2026-06-17 15:43:21 +0800

    refactor: src files out

--- src/schemas/common.ts
+++
@@ -1,7 +0,0 @@
-import * as s from '../schema-builder';
-import { precacheTypes } from './resources';
-
-export const createPrecacheBlock = () =>
-  s
-    .obj('Precache')
-    .fields(Object.entries(precacheTypes).map(([n, pattern]) => [n, s.str().pattern(pattern)]));
--- src/schemas/units/index.ts
+++
@@ -1,8 +0,0 @@
-import * as s from '../../schema-builder';
-import { hero, heroCustom } from './heroes';
-import { baseUnit, unitCustom } from './units';
-
-export const units = s.root().rest(baseUnit());
-export const unitsCustom = s.root().rest(unitCustom);
-export const heroes = s.root().rest(hero);
-export const heroesCustom = s.root().rest(heroCustom);
